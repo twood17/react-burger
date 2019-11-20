aa93f523727c0e37ad9d8e2de38baa192ae6dae7
Ty Wood <twood06@gmail.com>
2019-11-20 15:49:50 -0500

    first

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Person from './components/Person/Person';
 import './App.css';
 
 class App extends Component {
-	//testing 234
+	//testing 2345
 	state = {
 		persons: [ 
 			{ id: 'd14312', name: 'Ty', age: 31 }, 
